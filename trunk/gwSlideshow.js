5f5194b207c4063c63c756ebde1a097d30be6d43
henson.garth <henson.garth@da06a190-1e3a-11de-a108-cd2f117ce590>
2010-02-27 05:24:52 +0000

    Restructured to be fully OOP supported. Also added event listener capabilities and support for before and after rotation (named 'beforerotate' and 'rotate' respectively)

--- trunk/gwSlideshow.js
+++ trunk/gwSlideshow.js
@@ -6,181 +6,314 @@
  * please feel free to contact me at garth [at] guahanweb [dot] com.
  *
  * @author Garth Henson (Guahan Web)
- * @version 0.1
+ * @version 0.2
  */
-$.fn.gwSlideshow = function(options)
-{
-	// First, prime the style and variables we will need throughout
-	var opts = $.extend({
-		width  : '300px',
-		height : '80px',
-		border : '1px solid #000000',
-		bg_color : '#ffffff',
-		delay : 5000,
-		transition : 'fade',
-		transition_speed : 500,
-		start : 0,
-		shuffle : false
-	}, options);
-	
-	// Set up the visibility and layout of the containment unit
-	$(this).css({
-		width  : opts.width,
-		height : opts.height,
-		border : opts.border,
-		backgroundColor : opts.bg_color,
-		position : 'relative',
-		overflow : 'hidden'
-	});
-	
-	if (opts.float)
+(function($) {
+	var gwSlideshow = function (element, options)
 	{
-		$(this).css('float', opts.float);
-	}
-	
-	// Find all slides and set them up for rotation
-	var slides = [];
-	$(this).children('div').each(function()
-	{
-		if ($(this).hasClass('slide'))
-		{
-			var s = $(this).css({
-				width  : opts.width,
-				height : opts.height,
-				backgroundColor : opts.bg_color,
-				position : 'absolute',
-				top  : 0,
-				left : 0,
-				overflow : 'hidden',
-				zIndex : 1
-			}).hide();
-			
-			slides.push(s);
-		}
-	});
+		element = $(element);
+		var self = this;
 
-	// Now, let's set up the functions we will need for the rotations
-	
-	/**
-	 * Shuffle the slide array
-	 *
-	 * @return void
-	 */
-	function shuffle()
-	{
-		var o = slides;
-		for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-		slides = o;
-	}
-	
-	/**
-	 * Shows the next (+1) slide in the array
-	 *
-	 * @param {int} num The key of the currently visible slide
-	 * @return void
-	 */
-	function showNext(num)
-	{
-		var next = num + 1;
-		if (next == slides.length)
+		/**
+		 * Initialization of the slideshow object. Here is where the settings are saved and all elements
+		 * are parsed and manipulated as necessary
+		 *
+		 * @return void
+		 */
+		self.init = function (options)
 		{
-			next = 0;
-		}
-		
-		doRotation(num, next);
-	}
-	
-	/**
-	 * Shows the previous (-1) slide in the array
-	 *
-	 * @param {int} num The key of the currently visible slide
-	 * @return void
-	 */
-	function showPrev(num)
-	{
-		var next = num - 1;
-		if (next < 0)
-		{
-			next = slides.length;
-		}
-		
-		doRotation(num, next);
-	}
-	
-	/**
-	 * Fades from slide num1 to num2 and performs provided callback upon completion
-	 *
-	 * @param {int} num1 The slide to fade out
-	 * @param {int} num2 The slide to fade in
-	 * @param {function} callback Optional callback function to execute
-	 * @return void
-	 */
-	function doFade(num1, num2, callback)
-	{
-		slides[num1].fadeOut('slow');
-		slides[num2].fadeIn('slow', callback);
-	}
-	
-	/**
-	 * Slides slide num2 over the currently visible slide
-	 *
-	 * @param {int} num The slide to display
-	 * @param {function} callback Optional callback function to execute
-	 * @return void
-	 */
-	function doSlide(num, callback)
-	{
-		// Bring to front
-		var zmax = 1;
-		for (var i = 0; i < slides.length; i++)
-		{
-			var cur = slides[i].css('zIndex');
-			zmax = (cur > zmax) ? cur : zmax;
-		}
-		slides[num].css('zIndex', zmax + 1);
-		
-		slides[num].css('top', '-' + opts.height).show();
-		slides[num].animate({
-			top : 0
-		}, opts.transition_speed, callback);
-	}
-	
-	/**
-	 * Executes the transition from slide num1 to num2, taking into account the option values.
-	 *
-	 * @param {int} num1 The slide from which to transition
-	 * @param {int} num2 The slide to which to transition
-	 * @return void
-	 */
-	function doRotation(num1, num2)
-	{
-		// Only attempt rotation if there is more than one slide
-		if (slides.length > 1)
-		{ 
-			setTimeout(function()
+			self.settings = $.extend({
+				width  : '300px',
+				height : '80px',
+				border : '1px solid #000000',
+				bg_color : '#ffffff',
+				delay : 5000,
+				transition : 'fade',
+				transition_speed : 500,
+				start : 0,
+				shuffle : false
+			}, options || {});
+
+			self.listeners = {};
+			self.setupContainer();
+			self.setupSlides();
+
+			// Initialize the first slide, and start the rotation, shuffling if called for
+			if (self.settings.shuffle)
 			{
-				var callback = function()
+				self.shuffle();
+			}
+			self.slides[self.settings.start].show();
+			self.showNext(self.settings.start);
+		};
+
+		/**
+		 * Set up the styles for the container
+		 *
+		 * @return void
+		 */
+		self.setupContainer = function ()
+		{
+			element.css({
+				width  : self.settings.width,
+				height : self.settings.height,
+				border : self.settings.border,
+				backgroundColor : self.settings.bg_color,
+				position : 'relative',
+				overflow : 'hidden'
+			});
+
+			if (self.settings.float || false)
+			{
+				element.css('float', self.settings.float);
+			}
+		};
+
+		/**
+		 * Find and position all slides for the show
+		 *
+		 * @return void
+		 */
+		self.setupSlides = function ()
+		{
+			self.slides = [];
+			element.children('div').each(function()
+			{
+				if ($(this).hasClass('slide'))
 				{
-					showNext(num2);
-				};
-				
-				if (opts.transition == 'fade')
-				{
-					doFade(num1, num2, callback);
+					var s = $(this).css({
+						width  : self.settings.width,
+						height : self.settings.height,
+						backgroundColor : self.settings.bg_color,
+						position : 'absolute',
+						top  : 0,
+						left : 0,
+						overflow : 'hidden',
+						zIndex : 1
+					}).hide();
+			
+					self.slides.push(s);
 				}
-				else if (opts.transition == 'slide')
-				{
-					doSlide(num2, callback);
-				}
-			}, opts.delay);
-		}
-	}
+			});
+		};
+
+		/**
+		 * Shuffle the slide array
+		 *
+		 * @return void
+		 */
+		self.shuffle = function ()
+		{
+			var o = self.slides;
+			for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+			self.slides = o;
+		};
+
+		/**
+		 * Shows the next (+1) slide in the array
+		 *
+		 * @param {int} num The key of the currently visible slide
+		 * @return void
+		 */
+		self.showNext = function (num)
+		{
+			var next = num + 1;
+			if (next == self.slides.length)
+			{
+				next = 0;
+			}
+			
+			self.doRotation(num, next);
+		};
 	
-	// Initialize the first slide, and start the rotation, shuffling if called for
-	if (opts.shuffle)
+		/**
+		 * Shows the previous (-1) slide in the array
+		 *
+		 * @param {int} num The key of the currently visible slide
+		 * @return void
+		 */
+		self.showPrev = function (num)
+		{
+			var next = num - 1;
+			if (next < 0)
+			{
+				next = self.slides.length;
+			}
+			
+			self.doRotation(num, next);
+		};
+	
+		/**
+		 * Fades from slide num1 to num2 and performs provided callback upon completion
+		 *
+		 * @param {int} num1 The slide to fade out
+		 * @param {int} num2 The slide to fade in
+		 * @param {function} callback Optional callback function to execute
+		 * @return void
+		 */
+		self.doFade = function (num1, num2, callback)
+		{
+			self.slides[num1].fadeOut('slow');
+			self.slides[num2].fadeIn('slow', callback);
+		};
+	
+		/**
+		 * Slides slide num2 over the currently visible slide
+		 *
+		 * @param {int} num The slide to display
+		 * @param {function} callback Optional callback function to execute
+		 * @return void
+		 */
+		self.doSlide = function (num, callback)
+		{
+			// Bring to front
+			var zmax = 1;
+			for (var i = 0; i < self.slides.length; i++)
+			{
+				var cur = self.slides[i].css('zIndex');
+				zmax = (cur > zmax) ? cur : zmax;
+			}
+			self.slides[num].css('zIndex', zmax + 1);
+			
+			self.slides[num].css('top', '-' + self.settings.height).show();
+			self.slides[num].animate({
+				top : 0
+			}, self.settings.transition_speed, callback);
+		};
+		
+		/**
+		 * Executes the transition from slide num1 to num2, taking into account the option values.
+		 *
+		 * @param {int} num1 The slide from which to transition
+		 * @param {int} num2 The slide to which to transition
+		 * @return void
+		 */
+		self.doRotation = function (num1, num2)
+		{
+			// Only attempt rotation if there is more than one slide
+			if (self.slides.length > 1)
+			{ 
+				setTimeout(function()
+				{
+					self.triggerEvent('beforerotate', {prev_slide : num1, curr_slide : num2});
+
+					var callback = function()
+					{
+						self.showNext(num2);
+					};
+					
+					if (self.settings.transition == 'fade')
+					{
+						self.doFade(num1, num2, function ()
+						{
+							callback();
+							self.triggerEvent('rotate', {prev_slide : num1, curr_slide : num2});
+						});
+					}
+					else if (self.settings.transition == 'slide')
+					{
+						self.doSlide(num2, function ()
+						{
+							callback();
+							self.triggerEvent('rotate', {prev_slide : num1, curr_slide : num2});
+						});
+					}
+				}, self.settings.delay);
+			}
+		};
+
+		/**
+		 * Add a listener to an internal event
+		 *
+		 * @param {String} type The name of the event to target
+		 * @param {function} callback The function to execute
+		 * @return void
+		 */
+		self.addListener = function (type, callback)
+		{
+			// First, add empty array to listener object if it isn't there
+			if (!self.listeners[type] || true)
+			{
+				self.listeners[type] = [];
+			}
+
+			// Push the listener onto the stack
+			self.listeners[type].push(callback);
+		};
+
+		/**
+		 * Remove a listener from the internal event
+		 *
+		 * @param {String} type The name of the event to target
+		 * @param {function} callback The function to remove
+		 * @return void
+		 */
+		self.removeListener = function (type, callback)
+		{
+			if ((self.listeners[type] || false) && self.listeners[type].length > 0)
+			{
+				var temp = [];
+				for (var i = 0; i < self.listeners[type].length; i++)
+				{
+					if (self.listeners[type][i] != callback)
+					{
+						alert(callback + "\n\n NOT EQUAL TO \n\n" + self.listeners[type][0]);
+						temp.push(self.listeners[type][i]);
+					}
+				}
+				self.listeners[type] = temp;
+			}
+		};
+
+		/**
+		 * Execute the given event, executing all listeners
+		 *
+		 * @param {String} type The name of the event to trigger
+		 * @return void
+		 */
+		self.triggerEvent = function (type, params)
+		{
+			if ((self.listeners[type] || false) && self.listeners[type].length > 0)
+			{
+				// Handle only valid events
+				switch (type)
+				{
+					case 'beforerotate':
+						// Called with 3 params: <prev_slide>, <curr_slide>, <slideshow>
+						for (var i = 0; i < self.listeners[type].length; i++)
+						{
+							self.listeners[type][i](params.prev_slide, params.curr_slide, self);
+						}
+						break;
+	
+					case 'rotate':
+						// Called with 3 params: <prev_slide>, <curr_slide>, <slideshow>
+						for (var i = 0; i < self.listeners[type].length; i++)
+						{
+							self.listeners[type][i](params.prev_slide, params.curr_slide, self);
+						}
+						break;
+				}
+			}
+		};
+	
+		self.init(options);
+	};
+
+	// Let's actually declare the plugin now that we have our object 
+	$.fn.gwSlideshow = function(options)
 	{
-		shuffle();
-	}
-	slides[opts.start].show();
-	showNext(opts.start);
-};
+		return this.each(function ()
+		{
+			var element = $(this);
+
+			// Return early if this element already has a plugin instance
+			if (element.data('gwSlideshow')) return;
+
+			var slideshow = new gwSlideshow(this, options);
+
+			// Store plugin object in this element's data
+			element.data('gwSlideshow', slideshow);
+		});
+	};
+})(jQuery);
